6334f277ae9cbcc7880a0153426e64f781ec6740
Thomas Amlee <thomasamlee@gmail.com>
2020-01-03 20:56:57 -0600

    getting rid of while loop

--- test_adc.py
+++ test_adc.py
@@ -12,11 +12,11 @@ ads = ADS.ADS1015(i2c)
 
 # Create single-ended input on channel 0
 chan = AnalogIn(ads, ADS.P0)
+print(f"Raw:{chan.value}\tVoltage:{chan.voltage}V")
 
 
 # print("{:>5}\t{:>5}".format('raw', 'v'))
 
-while True:
-    print(f"Raw:{chan.value}\tVoltage:{chan.voltage}V")
-    # print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
-    time.sleep(0.5)
+# while True:
+#     # print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
+#     time.sleep(0.5)
