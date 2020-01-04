7b8d8cd498d716d4317bab6f32314aec42214cba
Thomas Amlee <thomasamlee@gmail.com>
2020-01-03 21:07:05 -0600

    playing with ads class

--- project/capture_data/read_channel.py
+++ project/capture_data/read_channel.py
@@ -23,22 +23,3 @@ def read_channel(adc, chan, g):
     adc.stop_adc()
 
     return values
-
-
-# Create the I2C bus
-i2c = busio.I2C(board.SCL, board.SDA)
-
-# Create the ADC object using the I2C bus
-ads = ADS.ADS1015(i2c)
-
-# Create single-ended input on channel 0
-chan = AnalogIn(ads, ADS.P0)
-
-# Create differential input between channel 0 and 1
-#chan = AnalogIn(ads, ADS.P0, ADS.P1)
-
-print("{:>5}\t{:>5}".format('raw', 'v'))
-
-while True:
-    print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
-    time.sleep(0.5)
--- test_adc.py
+++ test_adc.py
@@ -8,7 +8,11 @@ from adafruit_ads1x15.analog_in import AnalogIn
 i2c = busio.I2C(board.SCL, board.SDA)
 
 # Create the ADC object using the I2C bus
-ads = ADS.ADS1015(i2c)
+ads = ADS.ADS1015(i2c, gain=1, data_rate=860)
+
+print(f"ads rates:{ads.rates}")
+print(f"ads rate_config:{ads.rate_config}")
+print(f"ads bits:{ads.bits}")
 
 # Create single-ended input on channel 0
 chan = AnalogIn(ads, ADS.P0)
